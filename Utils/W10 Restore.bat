ea64c97411b35466eaca6fd2389e12127750aea1
José Rogério <joserogerio.net@gmail.com>
2020-02-19 19:45:54 -0300

    Update W10 Restore.bat

--- Utils/W10 Restore.bat
+++ Utils/W10 Restore.bat
@@ -1,3 +1,6 @@
-sfc /SCANNOW
+## Escanear, reparar, substituir e restaurar qualquer arquivo de sistema operacional danificado, ausente ou alterado.
+  sfc /SCANNOW
+## Localiza setores inválidos e recupera informações legíveis.
 chkdsk /R
-DISM /Online /Cleanup-image /Restorehealth
+## Caso Ocorra erro no SFC:
+  DISM /Online /Cleanup-image /Restorehealth
