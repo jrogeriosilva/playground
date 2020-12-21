01183690fae53c2afbf65eac62ec7af6ef215728
José Rogério <joserogerio.net@gmail.com>
2020-12-21 13:56:29 -0300

    Create GPG_KEY_MISS_RESOLV.sh

---
+++ Utils/GPG_KEY_MISS_RESOLV.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' | while read key
+do if ! [[ ${keys[*]} =~ "$key" ]]
+then
+        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "$key"; keys+=("$key")
+fi
+done
