703bca2ee8b5d7ded60fa754dab219050b005907
José Rogério <joserogerio.net@gmail.com>
2021-06-29 00:58:20 -0300

    Update Script

--- Utils/verify-compromised-passwords.sh
+++ Utils/verify-compromised-passwords.sh
@@ -1,1 +1,18 @@
-SHA1=$(echo -n "corsa" | openssl sha1); FIRST5=$(echo ${SENHA^^} | (cut -d ' ' -f2 | cut -c-5)); LAST=$(echo ${SENHA^^} | cut -d ' ' -f2 | cut -c6-) ;curl https://api.pwnedpasswords.com/range/${FIRST5} | grep ${LAST}
+#!/bin/sh
+LEAKEDPASS=""
+tput sc;
+echo -n "Senha: "
+read -s SENHA;
+tput rc;tput el;
+echo "";
+SHA1=$(echo -n ${SENHA} | openssl sha1);
+SHA1CUT=$(echo -n ${SHA1} | cut -d ' ' -f2);
+FIRST5=$(echo -n ${SHA1^^} | cut -d ' ' -f2 | cut -c-5);
+LAST=$(echo ${SHA1^^} | cut -d ' ' -f2 | cut -c6-);
+LEAKEDPASS=$(curl --silent https://api.pwnedpasswords.com/range/${FIRST5} | grep ${LAST});
+if [[ -z ${LEAKEDPASS} ]]
+then
+	printf "\e[32m\e[5mOk : ${SHA1CUT}";
+else
+	printf "\e[31m\e[5mVazada : ${LEAKEDPASS}";
+fi
